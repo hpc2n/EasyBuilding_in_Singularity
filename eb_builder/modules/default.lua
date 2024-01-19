e0224319e844c8dd0f534123f12da4d5475d17fa
Ake Sandgren <ake.sandgren@hpc2n.umu.se>
2024-01-19 10:15:57 +0100

    Update eb_builder module and scripts

--- eb_builder/modules/default.lua
+++ eb_builder/modules/default.lua
@@ -1,8 +1,8 @@
-whatis([[Description: Wrapper for EasyBuild for building using singularity container]])
+whatis([[Description: Wrapper for EasyBuild for building using apptainer container]])
 
 help([[
 This module provides a small wrapper script for EasyBuild for building using
-singularity container on the cvmfs master tree.
+apptainer container on the cvmfs master tree.
 ]])
 
 local root = "/hpc2n/eb/software/eb_builder/default"
@@ -11,10 +11,6 @@ conflict("eb_builder")
 
 if not isloaded("EasyBuild") then
     load("EasyBuild")
-end
-
-if not isloaded("singularity") then
-    load("singularity")
 end
 
 prepend_path("PATH", pathJoin(root, "bin"))
--- eb_builder/modules/develop.lua
+++ eb_builder/modules/develop.lua
@@ -1,8 +1,8 @@
-whatis([[Description: Wrapper for EasyBuild for building using singularity container]])
+whatis([[Description: Wrapper for EasyBuild for building using apptainer container]])
 
 help([[
 This module provides a small wrapper script for EasyBuild for building using
-singularity container on the cvmfs master tree.
+apptainer container on the cvmfs master tree.
 ]])
 
 local root = "/hpc2n/eb/software/eb_builder/develop"
@@ -22,10 +22,6 @@ else
 
     if not isloaded("EasyBuild-develop") then
         load("EasyBuild-develop")
-    end
-
-    if not isloaded("singularity") then
-        load("singularity")
     end
 
     prepend_path("PATH", pathJoin(root, "bin"))
